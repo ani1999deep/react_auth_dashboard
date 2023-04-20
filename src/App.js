053d5f266d91f1fe623aa841aed948a24d155f21
ani1999deep <anideepbhowmick1@gmail.com>
2023-04-20 13:00:05 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Home from "./components/pages/Home";
 import Layout from "./components/pages/Layout";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginReg from "./components/pages/auth/LoginReg";
+import SendPasswordResetEmail from "./components/pages/auth/SendPasswordResetEmail";
+import ResetPassword from "./components/pages/auth/ResetPassword";
+import { Dashboard } from "@mui/icons-material";
 
 function App() {
   return (
@@ -14,7 +17,12 @@ function App() {
           <Route index element={<Home/>}/>
           <Route path="contact" element={<Contact/>}/>
           <Route path="login" element={<LoginReg/>}/>
+          <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
+          <Route path="reset" element={<ResetPassword />} />
+         
           </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/pages/auth/UserLogin.js
+++ src/components/pages/auth/UserLogin.js
@@ -1,7 +1,16 @@
 import {TextField,Button,Box,Alert} from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink,useNavigate } from 'react-router-dom';
 
 const UserLogin = () => {
+
+    const[error,setError]=useState({
+        status:false,
+        msg:'',
+        type:''
+    })
+
+    const navigate=useNavigate();
 
     const handleSubmit=(e)=>{
         e.preventDefault();
@@ -10,7 +19,18 @@ const UserLogin = () => {
             email:data.get('email'),
             password:data.get('password'),
         }
-        console.log(actualData)
+        
+
+        if(actualData.email && actualData.password){
+            console.log(actualData);
+            // For Clear the data,after submit
+            document.getElementById('login-form').reset();
+            setError({status:true,msg:'Login Success',type:'success'})
+
+            navigate('/dashboard')
+        }else{
+            setError({status:true,msg:'All fields are Required',type:'error'})
+        }
     }
   return <>
  <Box component='form' noValidate sx={{mt:1}} id='login-form' onSubmit={handleSubmit}>
@@ -20,7 +40,10 @@ const UserLogin = () => {
 <Box textAlign='center'>
     <Button type='submit' variant='contained' sx={{mt:3,mb:2,px:5}}>Login</Button>
 </Box>
-<NavLink to='/'>Forgot Password?</NavLink>
+<NavLink to='/sendpasswordresetemail'>Forgot Password?</NavLink>
+{
+    error.status ? <Alert severity={error.type}>{error.msg}</Alert> :''
+}
  </Box>
   </>
 }
